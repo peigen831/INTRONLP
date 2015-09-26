50fd1703d6775ee5d8daf5380c64ce2199c4c185
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-09-26 21:28:17 +0800

    Added Word for the words, hehehe. And some notes and functions for the algorithm

--- src/assignment4tagsa/TagsaParser.java
+++ src/assignment4tagsa/TagsaParser.java
@@ -1,46 +1,94 @@
 package assignment4tagsa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import common.Date;
 import common.FileWriter;
 import common.Parser;
 
 public class TagsaParser extends Parser {
-	
-	private final String CONSONANTS = "BCDFGHJKLMNPQRSTVWXYZÑbcdfghjklmnpqrstvwxyzñ";
-	private final String VOWELS = "AEIOUaeiou";
-	private final String[] PREFIX = { "i?", "ka?", "ma?", "mag?", "mang?", "na?", "nag?", "nang?", "pa?", "pag?", "pang?" };
-	private final String[] SUFFIX = { "?in", "?an", "?hin", "?han" };
-	private final String[] INFIX = { "?um?", "?in?" };
-	
-	TagsaParser() {
-		super();
-	}
-	
-	TagsaParser(String name, Date date, String body) {
-		super(name, date, body);
-	}
+    
+    private final String CONSONANTS = "BCDFGHJKLMNPQRSTVWXYZÑbcdfghjklmnpqrstvwxyzñ";
+    private final String VOWELS = "AEIOUaeiou";
+    private final String[] PREFIX = { "i?", "ka?", "ma?", "mag?", "mang?", "na?", "nag?", "nang?", "pa?", "pag?", "pang?" };
+    private final String[] SUFFIX = { "?in", "?an", "?hin", "?han" };
+    private final String[] INFIX = { "?um?", "?in?" };
+    
+    private List<String> foundPrefixes;
+    private List<String> foundSuffixes;
+    private List<String> foundInfixes;
+    private List<Word> words;
+    
+    TagsaParser() {
+        super();
+    }
+    
+    TagsaParser(String name, Date date, String body) {
+        super(name, date, body);
+    }
 
-	@Override
-	public void parse(String rawText) {
-		try{
-			// TODO parse
-			FileWriter fw = FileWriter.getInstance();
-			fw.writeLine(rawText);
-			fw.writeLine("");
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void parse(String rawText) {
+    	words = new ArrayList<>();
+        try{
+        	// parse each word
+            String[] allWords = rawText.split("[.,?!(): ]+");
+            for (String sWord : allWords) {
+            	foundPrefixes = new ArrayList<>();
+            	foundSuffixes = new ArrayList<>();
+            	foundInfixes = new ArrayList<>();
+            	Word word = new Word(sWord);
+                
+            	// TODO Step 1 - check if in dictionary; if yes, stop; else continue
+                // TODO Step 2 - check if hyphenated; if yes, is it a compound word or a prefix; if no, continue
+            	// TODO Step 3 - check if has infix e.g. kINawayan; if yes, separate infix
+            	
+            	words.add(word);
+            }
+            FileWriter fw = FileWriter.getInstance();
+            fw.writeLine(rawText);
+            fw.writeLine("");
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
-	@Override
-	public void parse(String name, Date date, String body) {
-		try{
-			FileWriter fw = FileWriter.getInstance();
-			fw.writeLine("Result from " + date.getYear() + " - " + date.getMonthName() + ":");
-			
-			parse(body);
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void parse(String name, Date date, String body) {
+        try{
+            FileWriter fw = FileWriter.getInstance();
+            fw.writeLine("Result from " + date.getYear() + " - " + date.getMonthName() + ":");
+            
+            parse(body);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    
+    private void inDictionary(String word) { /* TODO */ }
+    
+    private void hasHyphen(String word) { /* TODO */ }
+    
+    private void hasPrefix(String word) { /* TODO */ }
+    
+    private void hasSuffix(String word) { /* TODO */ }
+    
+    private void hasInfix(String word) { /* TODO */ }
+    
+    private void hasPartialDuplicate(String word) { /* TODO */ }
+    
+    private void hasFullDuplicate(String word) { /* TODO */ }
+    
+    private void foundHyphen(String word) { /* TODO */ }
+    
+    private void foundPrefix(String word) { /* TODO */ }
+    
+    private void foundSuffix(String word) { /* TODO */ }
+    
+    private void foundInfix(String word) { /* TODO */ }
+    
+    private void foundPartialDuplicate(String word) { /* TODO */ }
+    
+    private void foundFullDuplicate(String word) { /* TODO */ }
 }
---
+++ src/assignment4tagsa/Word.java
@@ -0,0 +1,94 @@
+package assignment4tagsa;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Word {
+	
+	private String originalWord;
+	private String rootWord;
+	private List<String> prefixes;
+	private List<String> suffixes;
+	private List<String> infixes;
+	
+	public Word(String originalWord) {
+		this.originalWord = originalWord;
+		rootWord = null;
+		prefixes = new ArrayList<>();
+		suffixes = new ArrayList<>();
+		infixes = new ArrayList<>();
+	}
+	
+	public String getOriginalWord() {
+		return originalWord;
+	}
+	
+	public String getRootWord() {
+		return rootWord;
+	}
+	
+	public String[] getPrefixes() {
+		return prefixes.toArray(new String[prefixes.size()]);
+	}
+	
+	public String[] getSuffixes() {
+		return suffixes.toArray(new String[suffixes.size()]);
+	}
+	
+	public String[] getInfixes() {
+		return infixes.toArray(new String[infixes.size()]);
+	}
+	
+	public String getPrefix(int index) {
+		return prefixes.get(index);
+	}
+	
+	public String getSuffix(int index) {
+		return suffixes.get(index);
+	}
+	
+	public String getInfix(int index) {
+		return infixes.get(index);
+	}
+	
+	public void setRootWord(String rootWord) {
+		this.rootWord = rootWord;
+	}
+	
+	public void setPrefixes(String[] prefixes) {
+		this.prefixes = new ArrayList<>(Arrays.asList(prefixes));
+	}
+	
+	public void setSuffixes(String[] suffixes) {
+		this.suffixes = new ArrayList<>(Arrays.asList(suffixes));
+	}
+	
+	public void setInfixes(String[] infixes) {
+		this.infixes = new ArrayList<>(Arrays.asList(infixes));
+	}
+	
+	public void setPrefixes(ArrayList<String> prefixes) {
+		this.prefixes = new ArrayList<>(prefixes);
+	}
+	
+	public void setSuffixes(ArrayList<String> suffixes) {
+		this.suffixes = new ArrayList<>(suffixes);
+	}
+	
+	public void setInfixes(ArrayList<String> infixes) {
+		this.infixes = new ArrayList<>(infixes);
+	}
+	
+	public void addPrefix(String prefix) {
+		prefixes.add(prefix);
+	}
+	
+	public void addSuffix(String suffix) {
+		suffixes.add(suffix);
+	}
+	
+	public void addinfix(String infix) {
+		infixes.add(infix);
+	}
+}
