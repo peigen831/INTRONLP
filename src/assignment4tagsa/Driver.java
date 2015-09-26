172b33e231c117fffd9ed25b2821e9cedadfbe55
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-09-26 10:17:18 +0800

    Copied Driver and commented out errors

---
+++ src/assignment4tagsa/Driver.java
@@ -0,0 +1,16 @@
+package assignment4tagsa;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		
+		//XMLParser parser = new XMLParser();
+		
+		//error occur because of XML &???
+		System.out.println("Processsing...");
+		
+		//parser.start();
+		
+		System.out.println("Done");
+	}
+}
