9c4041d9eb853c51b59f8392de0eb31979b89ced
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-09-26 22:00:56 +0800

    Added comments to the functions for the algorithm

--- src/assignment4tagsa/TagsaParser.java
+++ src/assignment4tagsa/TagsaParser.java
@@ -66,32 +66,109 @@ public class TagsaParser extends Parser {
         }
     }
     
+    /**
+     * Checks if the word is found in the dictionary
+     * @param String word
+     * @return true if the word is in the dictionary;
+     * 		   false otherwise
+     */
     private boolean inDictionary(String word) { /* TODO */ return false; }
     
+    /**
+     * Checks if the word contains a hyphen
+     * @param String word
+     * @return true if the word has a hyphen;
+     * 		   false otherwise
+     */
     private boolean hasHyphen(String word) { /* TODO */ return false; }
     
+    /**
+     * Checks if the word contains a prefix
+     * @param String word
+     * @return true if the word has a prefix;
+     * 		   false otherwise
+     */
     private boolean hasPrefix(String word) { /* TODO */ return false; }
     
+    /**
+     * Checks if the word contains a suffix
+     * @param String word
+     * @return true if the word has a suffix;
+     * 		   false otherwise
+     */
     private boolean hasSuffix(String word) { /* TODO */ return false; }
     
+    /**
+     * Checks if the word contains a infix
+     * @param String word
+     * @return true if the word has a infix;
+     * 		   false otherwise
+     */
     private boolean hasInfix(String word) { /* TODO */ return false; }
     
+    /**
+     * Checks if the word contains a partial duplicate
+     * @param String word
+     * @return true if the word has a partial duplicate;
+     * 		   false otherwise
+     */
     private boolean hasPartialDuplicate(String word) { /* TODO */ return false; }
     
+    /**
+     * Checks if the word contains a full duplicate
+     * @param String word
+     * @return true if the word has a full duplicate;
+     * 		   false otherwise
+     */
     private boolean hasFullDuplicate(String word) { /* TODO */ return false; }
     
+    /**
+     * Processes the word to split the hyphenated words or remove
+     * the prefix and check if the result is acceptable
+     * @param String word
+     */
     private void processWordWithHyphen(String word) { /* TODO */ }
     
+    /**
+     * Processes the word to remove the prefix and check
+     * if the result is acceptable
+     * @param String word
+     */
     private void processWordWithPrefix(String word) { /* TODO */ }
     
+    /**
+     * Processes the word to remove the suffix and check
+     * if the result is acceptable
+     * @param String word
+     */
     private void processWordWithSuffix(String word) { /* TODO */ }
     
+    /**
+     * Processes the word to remove the infix and check
+     * if the result is acceptable
+     * @param String word
+     */
     private void processWordWithInfix(String word) { /* TODO */ }
     
+    /**
+     * Processes the word to remove the partial duplicate and check
+     * if the result is acceptable
+     * @param String word
+     */
     private void processWordWithPartialDuplicate(String word) { /* TODO */ }
     
+    /**
+     * Processes the word to remove the full duplicate and check
+     * if the result is acceptable
+     * @param String word
+     */
     private void processWordWithFullDuplicate(String word) { /* TODO */ }
     
+    /**
+     * Finds the first candidate that is found in the dictionary
+     * @param String[] candidates
+     * @return the best candidate
+     */
     private String findBestCandidate(String[] candidates) {
     	String bestCandidate = null;
     	for (String candidate : candidates) {
@@ -103,6 +180,16 @@ public class TagsaParser extends Parser {
     	return bestCandidate;
     }
     
+    /**
+     * Checks if the resulting word is "acceptable" wherein
+     * acceptable means that a word starting with a vowel has at
+     * least 3 letters and at least 1 consonant, while a word
+     * starting with a consonant has at least 4 letters and at
+     * least 1 vowel
+     * @param String word
+     * @return true if the word is an acceptable length;
+     *         false otherwise
+     */
     private boolean isAcceptable(String word) {
     	if (VOWELS.contains(word.charAt(0) + "")) {
     		if (word.length() >= 3 /* TODO && word contains at least 1 consonant */) {
