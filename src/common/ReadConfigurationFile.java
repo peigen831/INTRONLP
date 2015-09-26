2851990091e5cd3f84c4507ddba9b1d67a34efb6
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-09-26 10:02:41 +0800

    Prepared common/ReadConfigurationFile to be usable from any package

---
+++ src/common/ReadConfigurationFile.java
@@ -0,0 +1,61 @@
+package common;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+public class ReadConfigurationFile {
+	
+	private static final String CONFIGFILENAME = "config.properties";
+	private static String PACKAGENAME = null;
+	
+	public static void setPackage(String packageName) {
+		PACKAGENAME = packageName;
+	}
+	
+	public static String getProperty(String property) {
+		return getProperty(PACKAGENAME, property);
+	}
+	
+	public static String getProperty(String packageName, String property) {
+		Properties properties = readFile(CONFIGFILENAME);
+		
+		Enumeration<Object> enuKeys = properties.keys();
+		Map<String, String> propertyMap = new HashMap<>();
+		while (enuKeys.hasMoreElements()) {
+			String key = (String) enuKeys.nextElement();
+			String value = properties.getProperty(key);
+			propertyMap.put(key, value);
+		}
+		
+		return propertyMap.get(property);
+	}
+	
+	private static Properties readFile(String fileName) {
+		return readFile(PACKAGENAME, fileName);
+	}
+	
+	private static Properties readFile(String packageName, String fileName) {
+		try {
+			File file = new File("src/" + PACKAGENAME + "/" + fileName);
+			FileInputStream fileInput = new FileInputStream(file);
+			Properties properties = new Properties();
+			properties.load(fileInput);
+			fileInput.close();
+			
+			return properties;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+}
