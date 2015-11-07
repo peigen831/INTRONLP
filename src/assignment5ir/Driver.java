f2b867641aca6f509bd23bc44a61c07531fd9f9d
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-11-08 07:32:06 +0800

    Made a new package for the newest assignment

---
+++ src/assignment5ir/Driver.java
@@ -0,0 +1,26 @@
+package assignment5ir;
+
+import java.io.FileNotFoundException;
+
+import common.FileWriter;
+import common.XmlReader;
+
+public class Driver {
+	
+	private static String PACKAGENAME = "assignment4tagsa";
+	
+	public static void main(String[] args) {
+		// XmlReader reader = new XmlReader(PACKAGENAME, new TagsaParser());
+		try {
+			FileWriter.getNewInstance(PACKAGENAME, "Stemmered.txt").createNewFile();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Processsing...");
+		
+		// reader.start();
+		
+		System.out.println("Done");
+	}
+}
