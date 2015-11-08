c446df16cc967b65b1e1fbbd8210c2e0c284af1b
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-11-08 17:40:55 +0800

    Created new IrParser class for the new assignment

---
+++ src/assignment5ir/IrParser.java
@@ -0,0 +1,20 @@
+package assignment5ir;
+
+import common.Date;
+import common.Parser;
+
+public class IrParser extends Parser {
+	
+	@Override
+	public void parse(String rawText) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public void parse(String name, Date date, String body) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
