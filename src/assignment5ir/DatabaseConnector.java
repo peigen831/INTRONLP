e1d371f5cd22e8d234a1b5b918490b084a4c256c
Sharmaine Lim <SharmaineLim@users.noreply.github.com>
2015-11-08 15:13:29 +0800

    Refactored the database connector class

--- src/assignment5ir/DatabaseConnector.java
+++ src/assignment5ir/DatabaseConnector.java
@@ -2,19 +2,42 @@ package assignment5ir;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DatabaseConnector {
 	
+	private Connection con;
+	
 	public static void main( String args[] ) {
-		Connection c = null;
+		DatabaseConnector dbCon = new DatabaseConnector();
 		try {
-			Class.forName("org.sqlite.JDBC");
-			c = DriverManager.getConnection("jdbc:sqlite:test.db");
-		} catch ( Exception e ) {
-			System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-			System.exit(0);
+			dbCon.openConnection();
+			dbCon.closeConnection();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 		System.out.println("Opened database successfully");
+	}
+	
+	public DatabaseConnector() {
+		con = null;
+	}
+	
+	public Connection getConnection() {
+		return con;
+	}
+	
+	private Connection openConnection() throws SQLException, ClassNotFoundException {
+		if (con != null) {
+			closeConnection();
+		}
+		Class.forName("org.sqlite.JDBC");
+		con = DriverManager.getConnection("jdbc:sqlite:test.db");
+		return con;
+	}
+	
+	private void closeConnection() throws SQLException {
+		con.close();
 	}
 	
 }
