abd5e35fd6737d46a2e1ea1f9d2442ec3c153bd9
peigen831 <peigen831@yahoo.com>
2015-11-28 11:14:25 +0800

    seperate part of codes

--- src/mp/SParser.java
+++ src/mp/SParser.java
@@ -9,8 +9,8 @@ import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.HasWord;
 import edu.stanford.nlp.ling.Label;
 import edu.stanford.nlp.parser.lexparser.LexicalizedParser;
-import edu.stanford.nlp.process.DocumentPreprocessor;
 import edu.stanford.nlp.process.Tokenizer;
+import edu.stanford.nlp.semgraph.SemanticGraphFactory;
 import edu.stanford.nlp.trees.GrammaticalStructure;
 import edu.stanford.nlp.trees.GrammaticalStructureFactory;
 import edu.stanford.nlp.trees.Tree;
@@ -18,69 +18,78 @@ import edu.stanford.nlp.trees.TreebankLanguagePack;
 import edu.stanford.nlp.trees.TypedDependency;
 
 class SParser {
-
-  /** This example shows a few more ways of providing input to a parser.
-   *
-   *  Usage: ParserDemo2 [grammar [textFile]]
-   */
-  public static void main(String[] args) throws IOException {
+	
+	public String grammar;
+    public String[] options;
+    public LexicalizedParser lp;
+    public TreebankLanguagePack tlp;
+    public GrammaticalStructureFactory gsf;
     
-	// Initialize Models
-	String grammar = args.length > 0 ? args[0] : "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
-    String[] options = { "-maxLength", "80", "-retainTmpSubcategories" };
-    LexicalizedParser lp = LexicalizedParser.loadModel(grammar, options);
-    TreebankLanguagePack tlp = lp.getOp().langpack();
-    GrammaticalStructureFactory gsf = tlp.grammaticalStructureFactory();
-
-    Iterable<List<? extends HasWord>> sentences;
-    if (args.length > 1) {
-      DocumentPreprocessor dp = new DocumentPreprocessor(args[1]);
-      List<List<? extends HasWord>> tmp =
-        new ArrayList<List<? extends HasWord>>();
-      for (List<HasWord> sentence : dp) {
-        tmp.add(sentence);
-      }
-      sentences = tmp;
-    } else {
-      String sent2 = ("Ministries may develop and implement additional information security policies, standards and guidelines for use within their organization or for a specific information system or program.");
-      // Use the default tokenizer for this TreebankLanguagePack
-      Tokenizer<? extends HasWord> toke =
-        tlp.getTokenizerFactory().getTokenizer(new StringReader(sent2));
-      List<? extends HasWord> sentence2 = toke.tokenize();
-      
-      List<List<? extends HasWord>> tmp =
-        new ArrayList<List<? extends HasWord>>();
-      tmp.add(sentence2);
-      sentences = tmp;
+    public void initialize(){
+    	grammar = "edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz";
+        options = new String[]{ "-maxLength", "80", "-retainTmpSubcategories" };
+        lp = LexicalizedParser.loadModel(grammar, options);
+        tlp = lp.getOp().langpack();
+        gsf = tlp.grammaticalStructureFactory();
     }
+    
+    public Iterable<List<? extends HasWord>> tokenizeSentence(String[] sentences){
+		  Iterable<List<? extends HasWord>> result;
+		  List<List<? extends HasWord>> tmp = new ArrayList<List<? extends HasWord>>();
+		
+		  for(String s: sentences){
+			  Tokenizer<? extends HasWord> toke = tlp.getTokenizerFactory().getTokenizer(new StringReader(s));
+			  List<? extends HasWord> sTokenized = toke.tokenize();
+		  	  tmp.add(sTokenized);
+		  }
+		  
+		  result = tmp;
+		  return result;
+    }
+    
+    public void parseSentences(String[] input){
+        Iterable<List<? extends HasWord>> sTokenized = tokenizeSentence(input);
 
-    for (List<? extends HasWord> sentence : sentences) {
-    	// Parse for parts of speech
-      Tree parse = lp.parse(sentence);
-      parse.pennPrint();
-      System.out.println();
-      
-      // Find grammatical structure
-      GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
-      List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
-      System.out.println(tdl);
-      System.out.println();
+        for (List<? extends HasWord> sentence : sTokenized) {
+        	// Parse for parts of speech
+          Tree parse = lp.parse(sentence);
+          parse.pennPrint();
+          
+          // Find grammatical structure
+          GrammaticalStructure gs = gsf.newGrammaticalStructure(parse);
+          List<TypedDependency> tdl = gs.typedDependenciesCCprocessed();
+          System.out.println(tdl);
+          System.out.println();
 
-      System.out.println("The words of the sentence:");
-      for (Label lab : parse.labeledYield()) {
-        if (lab instanceof CoreLabel) {
-          System.out.println(((CoreLabel) lab).toString(CoreLabel.OutputFormat.VALUE_MAP));
-        } else {
-          System.out.println(lab);
+          System.out.println("The words of the sentence:");
+          for (Label lab : parse.yield()) {
+            if (lab instanceof CoreLabel) {
+            	System.out.println("Word: " + ((CoreLabel)lab).word() + " Tag:" +((CoreLabel)lab).tag());
+              //System.out.println(((CoreLabel) lab).toString(CoreLabel.OutputFormat.VALUE_MAP));
+            } else {
+              System.out.println("lab "+lab);
+            }
+          }
+          System.out.println(parse.taggedYield());
+
         }
-      }
-      System.out.println();
-      System.out.println(parse.taggedYield());
-      System.out.println();
-
     }
-  }
-
-  private SParser() {} // static methods only
-
+    
+    public static void main(String[] args) throws IOException {
+    	SParser s = new SParser();
+    	s.initialize();
+    	//2.1.1 Management must set direction and provide support for information security.
+    	//2.1.2 Implementation of information security activities across government must be coordinated by the Office of the Government Chief Information Officer.
+    	//2.1.6 Appropriate contacts shall be maintained with local law enforcement authorities, emergency support staff and service providers.
+    	//4.1.1 Security roles and responsibilities for personnel must be documented.
+    	//7.1.1 Access to information systems and services must be consistent with business needs and be based on security requirements.
+    	//7.3.3 Users must ensure the safety of sensitive information from unauthorized access, loss or damage.
+    	
+    	String[] input = {"Implementation of information security activities across government must be coordinated by the Office of the Government Chief Information Officer",
+    			"Appropriate contacts shall be maintained with local law enforcement authorities, emergency support staff and service providers.",
+    			"Security roles and responsibilities for personnel must be documented.",
+    			"Access to information systems and services must be consistent with business needs and be based on security requirements.",
+    			"Users must ensure the safety of sensitive information from unauthorized access, loss or damage."};
+    	s.parseSentences(input);
+    }
 }
